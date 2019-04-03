7d488c9a4e8369c166c5b58ff597ed9044dbdbd3
Fajar Adi <fjradi96@gmail.com>
2019-04-03 18:18:13 +0000

    protect database url to environment var

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var express                     = require("express"),
     cloudinary                  = require('cloudinary');
 
 //connect to mongodb
-mongoose.connect("mongodb+srv://admin:aihaibara14@cluster0-e2ftw.mongodb.net/YelpCamp?retryWrites=true");
+mongoose.connect(process.env.DATABASEURL);
 
 app.locals.moment = moment;
 
